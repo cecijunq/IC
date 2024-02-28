b6394b2c37087faabb88418b239d6da739a7f753
Cecilia Junqueira Vieira Machado Pereira <cecilia@MacBook-Air-de-Cecilia.local>
2024-02-28 18:05:56 -0300

    adicionados grafos + arquivos com legendas + listas de referências

--- parser_marisa.py
+++ parser_marisa.py
@@ -5,7 +5,6 @@ from urllib.request import urlopen
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
-import codecs 
 
 
 #Retira o elemento da pagina html
@@ -49,13 +48,10 @@ def get_vertices():
     # acessa cada arquivo .html para lê-lo e realizar o parser
     for file in list_files:
         nome_filme = file.replace(".html", "")
-        #print(nome_filme)
         film = path + '/' + file
     
         with open(film, 'r') as fp:
             content = fp.read()
-
-            #arr_article = []
 
             soup = BeautifulSoup(content, 'html.parser')
 
@@ -74,9 +70,77 @@ def get_vertices():
                         links.append(referenciado)
                         print(nome_filme, referenciado)
                         grafo.add_edge(filmes_indicados[nome_filme], filmes_indicados[referenciado])
-
-            #links = list(dict.fromkeys(links))
         
+    n_nos = grafo.number_of_nodes()
+    n_arestas = grafo.number_of_edges()
+
+    i = 0
+    md_out = 0
+    md_in = 0
+    max_out = 0
+    max_in = 0
+    min_out = 600
+    min_in = 600
+
+    for tuple in grafo.out_degree():
+        if i == 0:
+            min_out = tuple[1]
+            i=1
+        else:
+            if tuple[1] < min_out:
+                min_out = tuple[1]
+
+        n_outdegree.append(tuple[1])
+        md_out += tuple[1]
+        if tuple[1] > max_out:
+            max_out = tuple[1]
+
+    md_out = md_out / n_nos
+        
+    i = 0
+    for tuple in grafo.in_degree():
+        if i == 0:
+            min_in = tuple[1]
+            i=1
+        else:
+            if tuple[1] < min_in:
+                min_in = tuple[1]
+                
+        n_indegree.append(tuple[1])
+        md_in += tuple[1]
+        if tuple[1] > max_in:
+            max_in = tuple[1]
+
+    md_in = md_in / n_nos
+
+    grafo_ordenado = sorted(nx.strongly_connected_components(grafo), key=len, reverse=True)
+    giant_component = grafo.subgraph(grafo_ordenado[0])
+    diametro = nx.diameter(giant_component)
+
+    # calcula o betweenness médio
+    soma = 0.0
+    bet = nx.betweenness_centrality(grafo, normalized=False)
+    between = pd.DataFrame.from_dict(data=bet, orient='index')
+
+    for element in bet.values():
+        soma += element
+    betw_md = soma/grafo.number_of_nodes()
+
+    soma = 0.0
+    # calcula o closeness médio
+    close = nx.closeness_centrality(grafo)
+    closeness = pd.DataFrame.from_dict(data=close, orient='index')
+
+    for element in close.values():
+        soma += element
+    clo_md = soma/grafo.number_of_nodes()
+
+    densidade = nx.density(grafo)
+    transitividade = nx.transitivity(grafo)
+    pg_rnk = top_five_page_rank(nx.pagerank(grafo))
+
+    with open("./métricas_grafo.csv", "a") as to_write:
+        to_write.write("Português;{};{};{};{};{};{};{};{};{};{};{};{};{};{}\n".format(n_nos, n_arestas, diametro, max_in, min_in, max_out, min_out, md_in, md_out, betw_md, clo_md, densidade, transitividade, pg_rnk))
     nx.write_gexf(grafo, "grafo_pt.gexf")
     plt.show()
 
